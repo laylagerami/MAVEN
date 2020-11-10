3de16555d81ad3908a06d766ac6b026777038c58
laylagerami <laylagerami@github.com>
2020-11-10 11:07:47 +0000

    update

--- MoA_Viz/app.R
+++ MoA_Viz/app.R
@@ -1,102 +1,132 @@
-library(shiny)
-
-# Define UI for data upload app ----
-ui <- fluidPage(
-  
-  # App title ----
-  titlePanel("Uploading Files"),
-  
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    
-    # Sidebar panel for inputs ----
-    sidebarPanel(
+ui <- shinyUI(navbarPage("MoA Viz",
+       ### DATA TAB
+        tabPanel("Data Input",
+                 
+                 sidebarPanel(
+                   fileInput("smiles_or_target","Upload SMILES or list of targets",
+                             multiple=F),
       
-      # Input: Select a file ----
-      fileInput("file1", "Choose CSV File",
-                multiple = FALSE,
-                accept = c("text/csv",
-                           "text/comma-separated-values,text/plain",
-                           ".csv")),
-      
-      # Horizontal line ----
-      tags$hr(),
-      
-      # Input: Checkbox if file has header ----
-      checkboxInput("header", "Header", TRUE),
-      
-      # Input: Select separator ----
-      radioButtons("sep", "Separator",
-                   choices = c(Comma = ",",
-                               Semicolon = ";",
-                               Tab = "\t"),
-                   selected = ","),
-      
-      # Input: Select quotes ----
-      radioButtons("quote", "Quote",
-                   choices = c(None = "",
-                               "Double Quote" = '"',
-                               "Single Quote" = "'"),
-                   selected = '"'),
-      
-      # Horizontal line ----
-      tags$hr(),
-      
-      # Input: Select number of rows to display ----
-      radioButtons("disp", "Display",
-                   choices = c(Head = "head",
-                               All = "all"),
-                   selected = "head")
-      
-    ),
-    
-    # Main panel for displaying outputs ----
-    mainPanel(
-      
-      # Output: Data file ----
-      tableOutput("contents")
-      
-    )
-    
+                   radioButtons("targettype", "File type",
+                                choices = c(SMILES="smiles",
+                                            Targets="targetlist"),
+                                selected="smiles"),
+                   tags$hr(),
+                   
+                   fileInput("network","Upload network (.sif)",
+                             multiple=F),
+                   
+                   tags$hr(),
+                   
+                   fileInput("gex","Upload gene expression data (.txt)",
+                             multiple=F)
+                 ),
+                 
+                 mainPanel(
+                   tags$style(type="text/css",
+                              ".shiny-output-error { visibility: hidden; }",
+                              ".shiny-output-error:before { visibility: hidden; }"),
+                   tableOutput("target"),
+                   tableOutput("networkrender"),
+                   textOutput("networkstats"),
+                   tableOutput("gexdata"),
+                   textOutput("nextstep1")
+                 )
+                 
+                 ),
+        
+        
+        
+        
+        
+        ### ANALYSIS TAB
+        tabPanel("Targets",
+                 sidebarPanel(
+                   "Step 1. Target prediction with PIDGINv4"
+                   ,
+                   "Please specify parameters or leave as default",
+                   
+                   textInput("prob", "Probability threshold (0-1)", value = "0.75", width = NULL, placeholder = NULL),
+                   textInput("ad", "AD filter (0-1)", value = "0.8", width = NULL, placeholder = NULL),
+                   textInput("no_targets","Top number of targets to include",value="5"),
+                   textInput("ncores","Number of cores",value="10")
+                  
+                 ),
+                 mainPanel(
+                   textOutput("test")
+                 )
+                 )
+        )
   )
-)
 
-# Define server logic to read selected file ----
 server <- function(input, output) {
   
-  output$contents <- renderTable({
+  output$target <- renderTable({
+    file = input$smiles_or_target
+    ext <- tools::file_ext(file$datapath)
+    req(file)
+    validate(need(ext == "txt", "Please upload a txt file")) # if no .txt throws error
+    read.csv(file$datapath, header = F,sep="\t") # read the chosen file and display it in renderTable
+  })
+ 
+  # get network, as above
+ 
+  output$networkrender <- renderTable({
     
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, head of that data file by default,
-    # or all rows if selected, will be shown.
+    netfile=input$network
     
-    req(input$file1)
+    ext <- tools::file_ext(netfile$datapath)
     
-    # when reading semicolon separated files,
-    # having a comma separator causes `read.csv` to error
-    tryCatch(
-      {
-        df <- read.csv(input$file1$datapath,
-                       header = input$header,
-                       sep = input$sep,
-                       quote = input$quote)
-      },
-      error = function(e) {
-        # return a safeError if a parsing error occurs
-        stop(safeError(e))
-      }
-    )
+    req(file)
     
-    if(input$disp == "head") {
-      return(head(df))
+    validate(need(ext == "sif", "Please upload a .sif network file"))
+    
+    networkdf=read.csv(netfile$datapath,sep="\t")
+    
+    head(networkdf)
+  })
+  
+  # get number of nodes and edges
+  output$networkstats <- renderText({
+    
+    netfile=input$network
+    
+    ext <- tools::file_ext(netfile$datapath)
+    
+    req(file)
+    
+    networkdf=read.csv(netfile$datapath,sep="\t")
+    
+    g = igraph::graph_from_data_frame(networkdf)
+    
+    nodecount = igraph::gorder(g)
+    edgecount = igraph::gsize(g)
+    
+    paste0("Number of Nodes: ",round(nodecount,0),'  |  ',
+           "Number of Edges: ",round(edgecount,0))
+
+    
+    
+  })
+  
+  output$gexdata <- renderTable({
+    gexfile = input$gex
+    ext <- tools::file_ext(gexfile$datapath)
+    req(file)
+    validate(need(ext == "txt", "Please upload a txt file")) # if no .txt throws error
+    head(read.csv(gexfile$datapath, header = T,sep="\t")) # read the chosen file and display it in renderTable
+  })
+  
+  output$nextstep1 <- renderText({
+    if(!is.null(input$smiles_or_target) & !is.null(input$network) & !is.null(input$gex)){
+      "Data upload complete. Please move onto Targets!"
     }
-    else {
-      return(df)
-    }
-    
+  })
+  
+  output$test <- renderText({
+    t = input$ncores
+    "DO COMMAND LINE RUN AND VIS/DOWNLOAD AND OPTION TO SKIP PREDICTION"
   })
   
 }
 
-# Create Shiny app ----
 shinyApp(ui, server)
