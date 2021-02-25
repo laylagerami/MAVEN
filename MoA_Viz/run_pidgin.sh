71e3ec60a9667531e5e2cd029827054833317f6b
Layla <lh605@cam.ac.uk>
2021-02-25 14:59:11 +0000

    add detector of pidgin output and rendering

--- MoA_Viz/run_pidgin.sh
+++ MoA_Viz/run_pidgin.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source activate pidgin3_env
-python /scratch/software/PIDGINv4/predict.py -f /tmp/Rtmpnv3b5M/436ad22ce199d8ed078834f0/0.txt -d '	' --organism 'Homo' -b 10 --ad 75 -n 10 -o ./output/PIDGIN_10_75_10_2021-02-25_10:53:09.txt
+python 0/predict.py -f /var/folders/tx/syw_nk2n7fd0yk15gx73fsdm0000gp/T//Rtmp2eveyW/07d42c5cc904d7ced5e85ba5/0.txt -d '	' --organism 'Homo' -b 10 --ad 75 -n 10 -o ./output/PIDGIN_10_75_10_2021-02-25_14:41:00.txt
