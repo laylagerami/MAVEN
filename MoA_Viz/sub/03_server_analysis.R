b4eba14f7b60ad2e91e684318f4001ab8096c11b
Layla <lh605@cam.ac.uk>
2021-03-03 16:51:26 +0000

    Add TF/pathway calculations

--- MoA_Viz/sub/03_server_analysis.R
+++ MoA_Viz/sub/03_server_analysis.R
@@ -1,6 +1,3 @@
-output$test_select <- renderText({
-  paste0(input$dorothea_conf)
-})
 
 # Run DoRoTHEA
 observeEvent(input$run_dorothea, {
@@ -16,10 +13,13 @@ observeEvent(input$run_dorothea, {
     as.data.frame() %>% 
     rownames_to_column(var = "GeneID") %>%
     {colnames(.)[2]="NES"; .} %>%
-    dplyr::top_n(input$no_tfs, wt = abs(NES)) %>%
+    dplyr::top_n(as.numeric(input$no_tfs), wt = abs(NES)) %>%
     dplyr::arrange(NES) %>% 
     dplyr::mutate(GeneID = factor(GeneID))
   
+  output$tf_df = renderDT({
+    datatable(tf_activities_topn,options = list("pageLength" = 5))
+  })
   output$tf_plot = renderPlot({
     ggplot(tf_activities_topn,aes(x = reorder(GeneID, NES), y = NES)) + 
       geom_bar(aes(fill = NES), stat = "identity") +
@@ -35,4 +35,48 @@ observeEvent(input$run_dorothea, {
       xlab("Transcription Factors")
   })
 })
+
+# Progeny
+observeEvent(input$run_progeny, {
+  rownames(datadf) = datadf[,1]
+  datadf[,1] = NULL
+  datadf = as.matrix(datadf)
+  PathwayActivity_counts <<- progeny(datadf, scale=TRUE, 
+          organism="Human", perm=10000, z_scores=F,
+          top = as.numeric(input$no_genes_progeny))
+  PA_render <<- PathwayActivity_counts %>%
+    t() %>%
+    as.data.frame() %>%
+    tibble::rownames_to_column(var="Pathway")
+
+  output$progeny_df <- renderDT({
+    colnames(PA_render)[2] <- "score"
+    PA_render$score = as.numeric(as.character(PA_render$score))
+    PA_render %>%
+      arrange(desc(abs(score)))
+    datatable(PA_render,options=list("pageLength"=5))
+  })
+  
+  output$progeny_plot <- renderPlot({
+    PA_render <- PathwayActivity_counts %>%
+      t() %>%
+      as.data.frame() %>%
+      tibble::rownames_to_column(var="Pathway")
+    colnames(PA_render)[2] <- "score"
+    ggplot(PA_render,aes(x = reorder(Pathway, score), y = score)) + 
+      geom_bar(aes(fill = score), stat = "identity") +
+      scale_fill_gradient2(low = "darkblue", high = "indianred", 
+                           mid = "whitesmoke", midpoint = 0) + 
+      theme_minimal() +
+      theme(axis.title = element_text(face = "bold", size = 12),
+            axis.text.x = 
+              element_text(angle = 45, hjust = 1, size =10, face= "bold"),
+            axis.text.y = element_text(size =10, face= "bold"),
+            panel.grid.major = element_blank(), 
+            panel.grid.minor = element_blank()) +
+      xlab("Pathways")
+  })
+})
+
+
 
--- MoA_Viz/sub/03_ui_analysis.R
+++ MoA_Viz/sub/03_ui_analysis.R
@@ -1,50 +1,108 @@
-
 tabPanel("Analysis",
-         sidebarPanel(
-           h5("Choose Settings"),
-           tags$hr(),
-           "DoRothEA:",
-           checkboxGroupInput("dorothea_conf", label=h5("Confidence levels:",
-                      tags$style(type="text/css","#q7 {vertical-align: top}"),
-                      bsButton("q7", label = "", icon = icon("question"), style = "info", size = "extra-small")
-           ),
-                       c("A"="A",
-                         "B"="B",
-                         "C"="C",
-                         "D"="D",
-                         "E"="E"),
-                       selected=c("A","B","C"),
-                       inline=T),
-           bsPopover(id = "q7", title = "Confidence Levels",
-                     content = paste0("TF-gene interaction confidence levels to use for TF enrichment of gene expression data."),
-                     placement = "right", 
-                     trigger = "click", 
-                     options = list(container = "body")
-           ),
-           textInput("no_tfs", label = h5("Number of TFs to include",
-                                      tags$style(type = "text/css", "#q8 {vertical-align: top;}"),
-                                      bsButton("q8", label = "", icon = icon("question"), style = "info", size = "extra-small")
-           ), value = "20", width = NULL, placeholder = NULL),
-           bsPopover(id = "q8", title = "Number of TFs to include",
-                     content = paste0("Number of top enriched TFs to include as input to CARNIVAL."),
-                     placement = "right", 
-                     trigger = "click", 
-                     options = list(container = "body")
-           ),
-           actionButton("run_dorothea","Run DoRoThEA"),
-           tags$br(),
-           tags$br(),
-           "PROGENy: ",
-           tags$br(),
-           tags$br(),
-           "CARNIVAL"
-           
-         ),
-         
-         mainPanel(
-           plotOutput("tf_plot")
-         )
-         
-)
+  # Sidebar
+  sidebarPanel(width=4,
+  "Step 3: Carry out Analysis",
+  tags$br(),
+  tags$br(),
+  "Perform TF enrichment, pathway inference and causal reasoning.",
+  tags$br(),
+  tags$br(),
+  # Dorothea options
+  "DoRoThEA Options:",
+  # Conf level
+  checkboxGroupInput("dorothea_conf", label=h5("Confidence levels:",
+     tags$style(type="text/css","#q7 {vertical-align: top}"),
+     bsButton("q7", label = "", icon = icon("question"), style = "info", size = "extra-small")
+      ),
+    c("A"="A",
+      "B"="B",
+      "C"="C",
+      "D"="D",
+      "E"="E"),
+    selected=c("A","B","C"),
+    inline=T),
+    bsPopover(id = "q7", title = "Confidence Levels",
+              content = paste0("TF-gene interaction confidence levels to use for TF enrichment of gene expression data."),
+              placement = "right", 
+              trigger = "click", 
+              options = list(container = "body")
+    ),
+  # No TFS
+    textInput("no_tfs", label = h5("Number of TFs to include",
+     tags$style(type = "text/css", "#q8 {vertical-align: top;}"),
+     bsButton("q8", label = "", icon = icon("question"), style = "info", size = "extra-small")
+      ), value = "20", width = NULL, placeholder = NULL),
+    bsPopover(id = "q8", title = "Number of TFs to include",
+              content = paste0("Number of top enriched TFs to include as input to CARNIVAL."),
+              placement = "right", 
+              trigger = "click", 
+              options = list(container = "body")
+    ),
+  tags$br(),
+  tags$br(),
+  "PROGENy Options:",
+  # no. genes
+  textInput("no_genes_progeny", label = h5("Number of top responsive genes to include",
+                                 tags$style(type = "text/css", "#q8 {vertical-align: top;}"),
+                                 bsButton("q9", label = "", icon = icon("question"), style = "info", size = "extra-small")
+  ), value = "100", width = NULL, placeholder = NULL),
+  bsPopover(id = "q9", title = "Number of top responsive genes to include.",
+            content = paste0("Number of top differentially regulated genes to use for pathway analysis. This number can be increased depending on the coverage of your experiments. For instance, the number of quantified genes for single-cell RNA-seq is smaller than for Bulk RNA-seq or microarray. In those cases, we suggest to increase the number of responsive genes to 200-500."),
+            placement = "right", 
+            trigger = "click", 
+            options = list(container = "body")
+  )
+    ),
+    # Main panel has tabs inside
+    mainPanel(
+      tabsetPanel(
+        # DoRoThEA
+        tabPanel("DoRoThEA",
+          fluidRow(column(12,
+              h5("Run DoRoThEA and view plot"),
+              tags$hr(),
+              actionButton("run_dorothea","Run DoRoThEA"),
+              tags$br(),
+              tags$br(),
+              "Table of enriched TFs:",
+              DTOutput("tf_df"),
+              tags$br(),
+              tags$br(),
+              "Plot of enriched TFs:",
+              plotOutput("tf_plot")
+              
+                        )
+                 )
+        ),
+        tabPanel("Progeny",
+                 fluidRow(
+                   column(12,
+                  h5("Progeny"),
+                  tags$hr(),
+                  actionButton("run_progeny","Run PROGENy"),
+                  tags$br(),
+                  tags$br(),
+                  "Table of pathway scores:",
+                  DTOutput("progeny_df"),
+                  tags$br(),
+                  tags$br(),
+                  "Plot of pathway scores:",
+                  plotOutput("progeny_plot")
+                  
+                          )
+                 )
+                 ),
+        tabPanel("CARNIVAL",
+                 fluidRow(
+                   column(12,
+                          h5("CARNIVAL")
+                          )
+                 )
+            )
+      )
+    )
+  
+  )
+
 
 
