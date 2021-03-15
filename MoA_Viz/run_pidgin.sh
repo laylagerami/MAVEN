8790cc2879a600d0d69a12304c61191de455261b
Layla <lh605@cam.ac.uk>
2021-03-15 11:19:39 +0000

    Clean up

--- MoA_Viz/run_pidgin.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source activate pidgin3_env
-python /scratch/software/PIDGINv4/predict.py -f /tmp/RtmpVnMHYm/aabad1289c680ebdfc15bc9a/0.txt -d '	' --organism 'Homo' -b 10 --ad 50 -n 15 -o output/PIDGIN_10_50_15_2021-03-09_15:52:40
