2803539a645c2e2f4eb25f8fc67fd0cb3fcaa1b1
Layla <lh605@cam.ac.uk>
2021-03-15 15:08:10 +0000

    Create install_packages.R

---
+++ install_packages.R
@@ -0,0 +1,14 @@
+# Script to install packages required
+install_required <- function(x){
+  for( i in x ){
+    #  require returns TRUE invisibly if it was able to load package
+    if( ! require( i , character.only = TRUE ) ){
+      #  If package was not able to be loaded then re-install
+      install.packages( i , dependencies = TRUE )
+      #  Load package after installing
+      require( i , character.only = TRUE )
+    }
+  }
+}
+
+install_required(c("shiny","shinyjs","igraph","DT","miniUI","shinysky","shinyalert"))
