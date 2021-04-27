ec4fd91ff3c98bd4031abcfc7275ed8c36295578
Layla <lh605@cam.ac.uk>
2021-04-27 11:39:54 +0100

    add server/ui

--- server.R
+++ server.R
@@ -1,16 +1,38 @@
 # SERVER
 server = function(input, output, session) {
+  source("sub/00_server_welcome.R", local=T)
   source("sub/01_server_data.R", local=T)
   source("sub/02_server_targets.R",local=T)
   source("sub/03_server_analysis.R",local=T)
   source("sub/04_server_vis.R",local=T)
   source("sub/support_enrichment.R")
   source("sub/support_networks.R")
-  
 }
 
-# IMPROVEMENTS
-# slider for tf etc
-# advanced options for CARNIVAL
-# downloaders?
 
+# *DATA* = done!
+
+# *TARGETS*
+# sim to train  pending
+# display filename to user to check (file written to...)
+
+# *ANALYSIS*
+# Fix IBM link
+# Add options for other solvers
+## Put settings in log files for future reference (PIDGIN done, need to do the same for when run CARNIVAL is clicked including all vars)
+
+# *VISUALISATION*
+# lightn edges too
+# fix downloader disable button
+# deselect pathway
+# choose specific nodes to be enriched instead of all of them, lighten them in the network when deselcted
+
+# *OTHER*
+# bookmarking?
+## welcome page explaining each step (in progress, need to fill it in, do at the end)
+# more guidance for question marks
+# default 10 entries 
+
+# *DO LATER*
+# batch upload tab (upload multiple CARNIVAL networks)
+# generation of files to run batch uploads
--- ui.R
+++ ui.R
@@ -4,9 +4,10 @@ source("sub/global.R")
 ui = function(request) {
   fluidPage(
     useShinyjs(),
-    navbarPage(theme=shinytheme("flatly"),
+    navbarPage(theme=shinytheme("sandstone"),
       title="MAVEN",
       footer = column(12, align="center", "MAVEN-App 2021 (version: 0.1)"),
+      source("sub/00_ui_welcome.R")$value,
       source("sub/01_ui_data.R")$value,
       source("sub/02_ui_targets.R")$value,
       source("sub/03_ui_analysis.R")$value,
