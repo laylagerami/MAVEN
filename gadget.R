e0c5263ce43be16219e6e9730dc845930f65b997
Layla <lh605@cam.ac.uk>
2021-04-27 14:22:07 +0100

    gadget

--- gadget.R
+++ gadget.R
@@ -2,19 +2,17 @@ library(shiny)
 library(miniUI)
 library(chemdoodle)
 
-# Super-Simple Shiny App that shows how chemdoodle can be used within shiny.
-# 
-# Draw a molecule. When you hit the button, it will display the count of the molecules
-# in the sketcher frame.
-#
 
+# THIS SCRIPT IS WRITTEN BY GITHUB@ZACHCP 
 
-# shiny server function
-#
-server <- function(input, output) {
+server <- function(input, output,session) {
+  
+  session$onSessionEnded(function() {
+    stopApp()
+  })
   
   #set a dummy reactive variable
-  mol <- reactiveValues(moleculedata = NULL)
+  mol <- reactiveValues(moleculedata = NULL,smiles=NULL,molname=NULL,df=NULL)
   
   #function to update the value based on changes on the shiny side
   observeEvent(input$moleculedata, {
@@ -23,24 +21,57 @@ server <- function(input, output) {
     
   })
   
-  # output function simply tallies the atom counts
-  output$smiles <- renderText({
+  # output function renders SMILES
+  output$smiles_out <- renderText({
     if (is.null(mol$moleculedata)){
-      return("Draw a molecule and click Done!")
+      return("Draw a molecule and click Done to retrieve SMILES file")
     } else {
-      smiles <- toSmiles(mol$moleculedata)
-      return(paste("Smiles:", smiles))
+      mol$smiles <- toSmiles(mol$moleculedata)
+      return(paste("Smiles:", mol$smiles))
     }
   })
+  
+  # Render table
+  output$smiles_table <- renderTable({
+    if(!is.null(mol$moleculedata)){
+      if(input$comp_name==""){
+        mol$molname = "Compound"
+        df = t(data.frame(c(mol$smiles,"Compound 1")))
+        row.names(df) = NULL
+        colnames(df) = c("SMILES","Compound Name")
+        mol$df = df
+      }else{
+        mol$molname = input$comp_name
+        df = t(data.frame(c(mol$smiles,input$comp_name)))
+        row.names(df) = NULL
+        colnames(df) = c("SMILES","Compound Name")
+        mol$df = df
+      }
+      df
+    }
+  })
+  
+  output$download <- renderUI({
+    if(!is.null(mol$smiles)){
+      downloadButton("downloadSmiles", "Download .smi file")
+    }
+  })
+    
+  # Downloader
+  output$downloadSmiles <- downloadHandler(
+    filename = function() {
+      paste0(mol$molname, ".smi")
+    },
+    content = function(con) {
+      write.table(mol$df, con, row.names = FALSE, col.names=FALSE,sep="\t",quote=F)
+    }
+  )
 }
 
-
-# Shiny UI Function
-#
 ui <- miniPage(
   
-  mainPanel(h1(textOutput("smiles"))),
-  
+  mainPanel(h3(textOutput("smiles_out"))),
+  textInput("comp_name", "Compound ID (Optional)"),
   miniContentPanel(chemdoodle_sketcher(mol=NULL)),
   
   gadgetTitleBar("Draw A Molecule", right = miniTitleBarButton("done", "Done", primary = TRUE)),
@@ -50,13 +81,16 @@ ui <- miniPage(
               var mol = sketcher.getMolecule();
               var jsonmol = new ChemDoodle.io.JSONInterpreter().molTo(mol);
               Shiny.onInputChange("moleculedata", jsonmol);};'
-  )
+  ),
+  
+  # Render table
+  tableOutput("smiles_table"),
+  
+  # Download
+  uiOutput("download")
   
   )
 
-
-#' Call the Widget
-#'
 shinyApp(ui = ui, server = server)
 
 # save each SMLE in a DF and allow to download for input into app
